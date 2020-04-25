e905a938e90f0aa6f1fa65248e08122bbb07560a
imam riyadi <imamriyadi32@gmail.com>
2020-04-26 03:44:47 +0700

    add class dto (Data transfer object)

--- nestjs-task-management/src/tasks/tasks.controller.ts
+++ nestjs-task-management/src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
 import {Body, Controller, Get, Post} from '@nestjs/common';
 import {TasksService} from "./tasks.service";
 import {Task} from "./task.model";
+import {CreateTaskDto} from "./dto/create-task.dto";
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService:TasksService) {
@@ -12,10 +13,9 @@ export class TasksController {
     }
 
     @Post()
-    createTask(
-        @Body('title') title:string,
-        @Body('description') description:string,
-    ) : Task {
-       return this.tasksService.createTask(title,description)
+    createTask(@Body() createTaskDto:CreateTaskDto) : Task {
+       return this.tasksService.createTask(createTaskDto)
     }
+
+
 }
